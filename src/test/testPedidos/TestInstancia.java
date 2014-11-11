131ee1414b2616034347b7162916c318fe8de9fa
eitan <eitan@dpm>
2014-11-11 20:19:34 -0300

    Cambio de nombre de rama "pedidos" por rama "ramaDeMaster"

---
+++ src/test/testPedidos/TestInstancia.java
@@ -0,0 +1,14 @@
+package test.testPedidos;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestInstancia {
+
+	@Test
+	public void test() {
+		fail("No implementado aun");
+	}
+
+}
---
+++ src/test/testPedidos/TestRecorrido.java
@@ -0,0 +1,22 @@
+package test.testPedidos;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import clientes.Cliente;
+import pedidos.Instancia;
+import pedidos.Pedido;
+import pedidos.Recorrido;
+
+public class TestRecorrido {
+
+//	@Test
+//	public void test() {
+//		Cliente cliente = new Cliente(null, null, null, null, null, null);
+//		Pedido p = new Pedido(cliente, "1568975271", null, null, null, 255 ,524);
+//		Instancia inst = new Instancia(Set<Pedido>);
+//		Recorrido recorrer = new Recorrido(inst);
+//	}
+
+}
