37f38f4b28c5fc2dbf9aea8c5ad2f4a80cda3a4c
eitan <eitan@dpm>
2014-11-10 23:50:49 -0300

    Clases Instancia,Iterador,Recorrido,Pendientes(faltan los tests),me parece que la clase "Generador" no hace falta,ya que todo se implementa dentro de la clase Iterador

--- src/pedidos/Pendientes.java
+++ src/pedidos/Pendientes.java
@@ -1,6 +1,24 @@
 package pedidos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pendientes 
 {
-
+	Iterable<Pedido> pendientes;
+	
+	
+	public ArrayList<Pedido> enEspera()
+	{
+		ArrayList<Pedido> ret = null;
+		for(Pedido pedido : pendientes)
+		{
+			if (pedido.getEstado() == pedido.getEstado().Asignado)
+				ret.add(pedido);
+		}
+		
+		return ret;
+	}
+	
+	
 }
